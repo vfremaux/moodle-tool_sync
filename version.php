afb6453a9dfa8943691655ee256ce509ba9e8e38
valery <valery.fremaux@gmail.com>
2016-09-26 18:24:01 +0200

    automated sync from SVN

--- version.php
+++ version.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Flat file enrolment plugin version specification.
+ * Version details.
  *
  * @package    tool_sync
  * @copyright  2013 Valery Fremaux 
@@ -26,12 +26,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version  = 2016030801;
-$plugin->requires  = 2015111000;        // Requires this Moodle version
-$plugin->component = 'tool_sync';  // Full name of the plugin (used for diagnostics)
+$plugin->requires  = 2015111000;        // Requires this Moodle version.
+$plugin->component = 'tool_sync';  // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_RC;
-// $plugin->dependencies = array('block_publishflow' => 2012031800); // Needed for templating creation of new courses
 $plugin->release = '3.0.0 (Build 2016030801)';
 
-// Non Moodle fields
-// This fields will help overmanagement code builders without forcing upgrade to play
+// Non Moodle attributes.
 $plugin->codeincrement = '3.0.0003';
