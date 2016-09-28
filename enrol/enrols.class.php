86514d1be1187604a5c41ac3112b3ace903eb8ab
valery <valery.fremaux@gmail.com>
2016-09-28 16:08:46 +0200

    automated sync from SVN

--- enrol/enrols.class.php
+++ enrol/enrols.class.php
@@ -342,7 +342,7 @@ class enrol_sync_manager extends sync_manager {
                                         $this->feed_tryback($text);
                                     }
                                     $errorline = get_string('errorline', 'tool_sync')." $i :";
-                                    $errorline .= " $record['cmd'] role : $user->lastname $user->firstname ";
+                                    $errorline .= " {$record['cmd']} role : $user->lastname $user->firstname ";
                                     $errorline .= "== $role->shortname ==> $course->shortname";
                                     $this->report($errorline);
                                 } else {
