f7702b531c96c80d8fc928b01688dd28a4e94694
valery <valery.fremaux@gmail.com>
2017-01-22 13:56:10 +0100

    Load all working branches

--- version.php
+++ version.php
@@ -27,10 +27,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2016030802;
-$plugin->requires = 2014110400;        // Requires this Moodle version.
+$plugin->requires = 2015050500;        // Requires this Moodle version.
 $plugin->component = 'tool_sync';  // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_RC;
-$plugin->release = '2.8.0 (Build 2016030802)';
+$plugin->release = '2.9.0 (Build 2016030802)';
 
 // Non moodle attributes.
-$plugin->codeincrement = '2.8.0004';
+$plugin->codeincrement = '2.9.0004';
