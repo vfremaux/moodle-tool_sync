d6a2f5d975ce4fbee80d5b3b2158a47846f7ee86
valery <valery.fremaux@gmail.com>
2017-03-19 11:07:32 +0100

    fix unittests

--- enrols/externallib.php
+++ enrols/externallib.php
@@ -29,8 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-require_once("$CFG->libdir/externallib.php");
+require_once($CFG->libdir.'/externallib.php');
 require_once($CFG->dirroot.'/admin/tool/sync/lib.php');
+require_once($CFG->dirroot.'/enrole/externallibb.php');
 
 /**
  * Tool Sync control functions
