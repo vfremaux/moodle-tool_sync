8ffc37ae0ef7262b465a9c615d2d4d1f5e85ff99
valery <valery.fremaux@gmail.com>
2017-06-22 15:49:09 +0200

    add cohort context

--- version.php
+++ version.php
@@ -26,12 +26,12 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2017061501;
+$plugin->version = 2017061502;
 $plugin->requires = 2016051900; // Requires this Moodle version.
 $plugin->component = 'tool_sync'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_RC;
-$plugin->release = '3.1.0 (Build 2017061501)';
+$plugin->release = '3.1.0 (Build 2017061502)';
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.1.0008';
+$plugin->codeincrement = '3.1.0009';
 $plugin->privacy = 'dualrelease';
