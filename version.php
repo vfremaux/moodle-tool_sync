730f0f6e1b4d57e1c43647160ff9f8f7a0e240a0
valery <valery.fremaux@gmail.com>
2017-07-08 15:08:57 +0200

    fix bad cohort related function call for binding

--- version.php
+++ version.php
@@ -26,12 +26,12 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2017031700;
+$plugin->version = 2017061502;
 $plugin->requires = 2016112900; // Requires this Moodle version.
 $plugin->component = 'tool_sync'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_RC;
-$plugin->release = '3.2.0 (Build 2017031700)';
+$plugin->release = '3.2.0 (Build 2017061502)';
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.2.0010';
+$plugin->codeincrement = '3.2.0011';
 $plugin->privacy = 'dualrelease';
