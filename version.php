601ac41d99996501dfbe8a4699d4b11b86406300
valery <valery.fremaux@gmail.com>
2017-07-17 13:25:03 +0200

    fix cohort related errors

--- version.php
+++ version.php
@@ -26,11 +26,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2017061502;
+$plugin->version = 2017071700;
 $plugin->requires = 2016112900; // Requires this Moodle version.
 $plugin->component = 'tool_sync'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_RC;
-$plugin->release = '3.2.0 (Build 2017061502)';
+$plugin->release = '3.2.0 (Build 2017071700)';
 
 // Non moodle attributes.
 $plugin->codeincrement = '3.2.0011';
