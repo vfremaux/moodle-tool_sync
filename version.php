f3b4810cbf7d4f7e602ba5256a6ec11bb2efbc67
valery <valery.fremaux@gmail.com>
2017-07-18 11:36:53 +0200

    fix unstable version records

--- version.php
+++ version.php
@@ -26,12 +26,12 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2017071700;
+$plugin->version = 2017071701;
 $plugin->requires = 2016112900; // Requires this Moodle version.
 $plugin->component = 'tool_sync'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_RC;
-$plugin->release = '3.2.0 (Build 2017071700)';
+$plugin->release = '3.2.0 (Build 2017071701)';
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.2.0011';
+$plugin->codeincrement = '3.2.0012';
 $plugin->privacy = 'dualrelease';
