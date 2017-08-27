1a07c11b4f3940e1729b716f976a6b8783e0e042
valery <valery.fremaux@gmail.com>
2017-08-27 18:26:53 +0200

    add WS

--- version.php
+++ version.php
@@ -26,12 +26,12 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2017071701;
+$plugin->version = 2017073100;
 $plugin->requires = 2016112900; // Requires this Moodle version.
 $plugin->component = 'tool_sync'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_RC;
-$plugin->release = '3.2.0 (Build 2017071701)';
+$plugin->release = '3.2.0 (Build 2017073100)';
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.2.0012';
+$plugin->codeincrement = '3.2.0014';
 $plugin->privacy = 'dualrelease';
