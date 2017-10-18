a007f58c5730d5752201316e7f88988337cd22bc
valery <valery.fremaux@gmail.com>
2017-10-18 12:30:23 +0200

    Add input file wildcard processing

--- version.php
+++ version.php
@@ -33,5 +33,5 @@ $plugin->maturity = MATURITY_RC;
 $plugin->release = '3.2.0 (Build 2017091900)';
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.2.0016';
+$plugin->codeincrement = '3.2.0017';
 $plugin->privacy = 'dualrelease';
