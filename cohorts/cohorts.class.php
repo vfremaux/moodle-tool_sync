57126df1cd9b1806569745c1e6f6475226d47b13
valery <valery.fremaux@gmail.com>
2017-11-01 21:03:45 +0100

    add single user seek function

--- cohorts/cohorts.class.php
+++ cohorts/cohorts.class.php
@@ -176,7 +176,8 @@ class cohorts_sync_manager extends sync_manager {
             $this->report(get_string('cohortsstarting', 'tool_sync'));
 
             // We have no file to process. Probably because never setup.
-            if (!($filereader = $this->open_input_file($filerec))) {
+            if (!($filereader = $this->open_input_file($filerec, 'cohorts'))) {
+                set_config('lastrunning_cohorts', null, 'tool_sync');
                 return;
             }
 
@@ -206,12 +207,15 @@ class cohorts_sync_manager extends sync_manager {
             if (!tool_sync_check_separator($text)) {
                 // This is a column name line that should NOT contain any of other separators.
                 $this->report(get_string('invalidseparatordetected', 'tool_sync'));
+                set_config('lastrunning_cohorts', null, 'tool_sync');
                 return;
             }
 
+            $text = preg_replace('/\n?\r?$/', '', $text); // Remove a trailing end line.
             $headers = explode($syncconfig->csvseparator, $text);
 
             if (!$this->check_headers($headers, $required, $patterns, $metas, $optional, $optionaldefaults)) {
+                set_config('lastrunning_cohorts', null, 'tool_sync');
                 return;
             }
 
@@ -499,8 +503,6 @@ class cohorts_sync_manager extends sync_manager {
                 }
             }
             fclose($filereader);
-
-            $this->report('... finished.');
         }
 
         if ($this->execute == SYNC_COHORT_BIND_COURSES) {
@@ -532,7 +534,7 @@ class cohorts_sync_manager extends sync_manager {
             $metas = array();
 
             // We have no file to process. Probably because never setup.
-            if (!($filereader = $this->open_input_file($filerec))) {
+            if (!($filereader = $this->open_input_file($filerec, 'cohorts'))) {
                 return;
             }
 
@@ -623,8 +625,6 @@ class cohorts_sync_manager extends sync_manager {
             }
 
             fclose($filereader);
-
-            $this->report('... finished');
         }
 
         $trace = new \null_progress_trace();
@@ -648,6 +648,10 @@ class cohorts_sync_manager extends sync_manager {
         if ($DB->get_field('config_plugins', 'value', array('plugin' => 'tool_sync', 'name' => 'filefailed'))) {
             $this->write_tryback($filerec);
         }
+
+         // Free file lock.
+        set_config('lastrunning_cohorts', null, 'tool_sync');
+        $this->report("\n".get_string('endofreport', 'tool_sync'));
 
         return true;
     }
