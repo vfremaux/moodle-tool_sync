8d12e967df91c38296562257bbf8172c5fb50908
valery <valery.fremaux@gmail.com>
2017-11-08 23:17:06 +0100

    Automated sync from SVN

--- version.php
+++ version.php
@@ -26,12 +26,12 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2017091900;
+$plugin->version = 2017091901;
 $plugin->requires = 2015050500; // Requires this Moodle version.
 $plugin->component = 'tool_sync'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_RC;
-$plugin->release = '2.9.0 (Build 2017091900)';
+$plugin->release = '2.9.0 (Build 2017091901)';
 
 // Non moodle attributes.
-$plugin->codeincrement = '2.9.0017';
+$plugin->codeincrement = '2.9.0018';
 $plugin->privacy = 'dualrelease';
