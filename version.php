f506ddcf5946b80074662a155a215c91adc94c02
valery <valery.fremaux@gmail.com>
2018-03-15 11:25:53 +0100

    fix cohort default contextid when adding

--- version.php
+++ version.php
@@ -33,5 +33,5 @@ $plugin->maturity = MATURITY_RC;
 $plugin->release = '3.3.0 (Build 2017091901)';
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.3.0019';
+$plugin->codeincrement = '3.3.0020';
 $plugin->privacy = 'dualrelease';
