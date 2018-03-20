e84f4a2f980e9ef85a0cf29b43c59ece449a072d
valery <valery.fremaux@gmail.com>
2018-03-20 11:49:19 +0100

    resync code increment 0020

--- version.php
+++ version.php
@@ -29,7 +29,7 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version = 2017091901;
 $plugin->requires = 2014050800; // Requires this Moodle version.
 $plugin->component = 'tool_sync'; // Full name of the plugin (used for diagnostics).
-$plugin->maturity = MATURITY_RC;
+$plugin->maturity = MATURITY_STABLE;
 $plugin->release = '2.7.0 (Build 2017091900)';
 
 // Non moodle attributes.
