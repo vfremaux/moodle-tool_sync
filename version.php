5cfde7070122cd116073545cb0f2822d0e2b5c92
valery <valery.fremaux@gmail.com>
2018-05-25 15:51:01 +0200

    Fix lang call in settings

--- version.php
+++ version.php
@@ -30,8 +30,8 @@ $plugin->version = 2018052100;
 $plugin->requires = 2017110800; // Requires this Moodle version.
 $plugin->component = 'tool_sync'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.4.0 (Build 2017091901)';
+$plugin->release = '3.5.0 (Build 2017091901)';
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.4.0022';
+$plugin->codeincrement = '3.5.0022';
 $plugin->privacy = 'dualrelease';
