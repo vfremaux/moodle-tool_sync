db59dafe78693572d76928f47355cf0d0635a442
valery <valery.fremaux@gmail.com>
2018-06-12 11:36:39 +0200

    add group features

--- version.php
+++ version.php
@@ -33,5 +33,5 @@ $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '3.1.0 (Build 2017091901)';
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.1.0022';
+$plugin->codeincrement = '3.1.0023';
 $plugin->privacy = 'dualrelease';
