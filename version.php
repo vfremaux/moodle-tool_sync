eda35142856187e2d51bf3e12d88767b3b1073fa
valery <valery.fremaux@gmail.com>
2018-07-16 11:00:55 +0200

    Finish the groups GUI

--- version.php
+++ version.php
@@ -26,12 +26,12 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2018052101;
+$plugin->version = 2018052102;
 $plugin->requires = 2016051900; // Requires this Moodle version.
 $plugin->component = 'tool_sync'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.1.0 (Build 2018052101)';
+$plugin->release = '3.1.0 (Build 2018052102)';
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.1.0023';
+$plugin->codeincrement = '3.1.0024';
 $plugin->privacy = 'dualrelease';
