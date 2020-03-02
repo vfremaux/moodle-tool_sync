d38bf97e5264bbf9a8d2bef57dac10a0a28768b5
valery <valery.fremaux@gmail.com>
2020-03-02 10:13:18 +0100

    Automated sync from SVN

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2018100100;
-$plugin->requires = 2019112200; // Requires this Moodle version.
+$plugin->requires = 2019111200; // Requires this Moodle version.
 $plugin->component = 'tool_sync'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '3.8.0 (Build 2018100100)';
