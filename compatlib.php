40ea7c0da5476e0ad4c651b2cbff0918c59cf28d
valery <valery.fremaux@gmail.com>
2020-07-27 17:32:49 +0200

    Automated sync from SVN

--- compatlib.php
+++ compatlib.php
@@ -1,8 +1,28 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-// Prior 3.6
-// require_once($CFG->dirroot.'/lib/coursecatlib.php');
 
+/**
+ * Cross versions compatibility functions.
+ *
+ * @copyright 2008 valery.fremaux <valery.fremaux@gmail.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+// From 3.6
 function tool_sync_category_role_assignment_changed($roleid, $context) {
     return \core_course_category::role_assignment_changed($roleid, $context);
 }
@@ -10,4 +30,3 @@ function tool_sync_category_role_assignment_changed($roleid, $context) {
 function tool_sync_get_category($catid) {
     return \core_course_category::get($catid);
 }
-
